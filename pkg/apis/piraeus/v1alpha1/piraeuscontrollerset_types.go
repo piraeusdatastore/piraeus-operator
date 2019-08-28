36fd7b8fb088660c18b027a6fb83e938754429c0
Hayley Swimelar <hayley@linbit.com>
2019-08-28 10:03:03 -0700

    modify json representation of status structs

--- pkg/apis/piraeus/v1alpha1/piraeuscontrollerset_types.go
+++ pkg/apis/piraeus/v1alpha1/piraeuscontrollerset_types.go
@@ -39,12 +39,16 @@ type PiraeusControllerSetStatus struct {
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
 
+	// Keep the first letters in the json for Errors lowercase and all other
+	// statuses uppercase. This causes `kubectl get TYPE NAME -oyaml` to sort
+	// errors to below the other potentially very long Statuses which is the preferred UX.
+
 	// Errors remaining that will trigger reconciliations.
-	Errors []string
+	Errors []string `json:"errors"`
 	// ControllerStatus information.
-	ControllerStatus *NodeStatus
+	ControllerStatus *NodeStatus `json:"ControllerStatus"`
 	// SatelliteStatuses by hostname.
-	SatelliteStatuses map[string]*SatelliteStatus `json:"satelliteStatuses"`
+	SatelliteStatuses map[string]*SatelliteStatus `json:"SatelliteStatuses"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
