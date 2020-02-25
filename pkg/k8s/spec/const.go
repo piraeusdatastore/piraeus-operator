w00jay/knopic

Allows customized LINSTOR Satellite image

pkg/k8s/spec/const.go
@@ -64,10 +64,15 @@ const LinstorNSPriorityClassName = "linstor-ns-priority-class"
 const (
 	// LinstorServerImage is the repo/tag for linstor-server. The controller
 	// and node use the same image with different commands.
-	LinstorServerImage = "drbd.io/test-linstor-server"
+	LinstorServerImage = "drbd.io/linstor-controller"
 	// LinstorVersion must must match exactly in the ControllerSet and the NodeSet
 	// since the linstor controller and satellite versions must also match exactly.
-	LinstorVersion = "v1.2.1"
+	LinstorVersion = "v1.4.2.1"
+
+	// LinstorSatelliteImage is the repo/tag for LINSTOR Satellite contaier.
+	LinstorSatelliteImage = "drbd.io/linstor-satellite"
+	// LinstorSatelliteVersion is the release tag for the above image
+	LinstorSatelliteVersion = "v1.4.2"
 
 	// LinstorKernelModImage is the worker (aka satellite) image for each node
 	LinstorKernelModImage = "drbd.io/test-drbd"

piraeusdatastore/piraeus-operator

pkg/k8s/spec/const.go
@@ -67,7 +67,13 @@ const (
 	PiraeusServerImage = "quay.io/piraeusdatastore/piraeus-server"
 	// PiraeusVersion must must match exactly in the ControllerSet and the NodeSet
 	// since the linstor controller and satellite versions must also match exactly.
-	PiraeusVersion = "v1.2.1"
+	PiraeusVersion = "v1.4.2"
+
+	// PiraeusSatelliteImage is the repo/tag for LINSTOR Satellite contaier.
+	PiraeusSatelliteImage = "quay.io/piraeusdatastore/piraeus-server"
+	// PiraeusSatelliteVersion is the release tag for the above image
+	PiraeusSatelliteVersion = "v1.4.2"
+
 	// PiraeusKernelModImage is the worker (aka satellite) image for each node
 	PiraeusKernelModImage = "quay.io/piraeusdatastore/drbd9-centos7"
 	// PiraeusKernelModVersion is the release tag for the above image