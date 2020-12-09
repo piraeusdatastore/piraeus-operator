a2b3a338b6a558cecf3e250ea3fbf5665c8ac247
Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>
2020-12-09 14:03:46 +0100

    Let injector image fail if usermode_helper not disabled
    Using LINSTOR in containers without disabling DRBD usermode_helper will
    lead to "StandAlone" resources. A new setting was added in the
    kernel module injector images that checks that the parameter is configured
    for container environments.

--- pkg/k8s/spec/const.go
+++ pkg/k8s/spec/const.go
@@ -48,10 +48,12 @@ const (
 
 // Special strings for communicating with the module injector
 const (
-	LinstorKernelModHow            = "LB_HOW"
-	LinstorKernelModCompile        = "compile"
-	LinstorKernelModShippedModules = "shipped_modules"
-	LinstorKernelModDepsOnly       = "deps_only"
+	LinstorKernelModHow                = "LB_HOW"
+	LinstorKernelModCompile            = "compile"
+	LinstorKernelModShippedModules     = "shipped_modules"
+	LinstorKernelModDepsOnly           = "deps_only"
+	LinstorKernelModHelperCheck        = "LB_FAIL_IF_USERMODE_HELPER_NOT_DISABLED"
+	LinstorKernelModHelperCheckEnabled = "yes"
 )
 
 // Special strings when configuring Linstor
