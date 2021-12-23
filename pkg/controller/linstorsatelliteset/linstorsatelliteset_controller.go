piraeusdatastore/piraeus-operator

commit 2f0765a5a951027b3bfc6a1f348fa8f18bf96342

refactor: unify linstor client creation
In preparation for the CSI reconciler to also require access to the linstor
API, unify the creation of a LINSTOR client.

This requires touching some code that predates our linter, which requires
some renames and other minor changes.

Signed-off-by: Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>

pkg/controller/linstorsatelliteset/linstorsatelliteset_controller.go
@@ -276,23 +276,6 @@ func (r *ReconcileLinstorSatelliteSet) reconcileMonitoring(ctx context.Context, 
 	return drbdReactorCM, nil
 }
 
-func (r *ReconcileLinstorSatelliteSet) getLinstorClient(ctx context.Context, satelliteSet *piraeusv1.LinstorSatelliteSet) (*lc.HighLevelClient, error) {
-	log.Debug("get linstor client")
-
-	getSecret := func(secretName string) (map[string][]byte, error) {
-		secret := corev1.Secret{}
-
-		err := r.client.Get(ctx, types.NamespacedName{Name: secretName, Namespace: satelliteSet.Namespace}, &secret)
-		if err != nil {
-			return nil, err
-		}
-
-		return secret.Data, nil
-	}
-
-	return lc.NewHighLevelLinstorClientFromConfig(satelliteSet.Spec.ControllerEndpoint, &satelliteSet.Spec.LinstorClientConfig, getSecret)
-}
-
 func (r *ReconcileLinstorSatelliteSet) reconcileResource(ctx context.Context, satelliteSet *piraeusv1.LinstorSatelliteSet) error {
 	logger := log.WithFields(logrus.Fields{
 		"Name":      satelliteSet.Name,
@@ -397,13 +380,17 @@ func (r *ReconcileLinstorSatelliteSet) reconcileAllNodesOnController(ctx context
 
 	logger.Debug("ensure LINSTOR controller is reachable")
 
-	linstorClient, err := r.getLinstorClient(ctx, satelliteSet)
+	linstorClient, err := lc.NewHighLevelLinstorClientFromConfig(
+		satelliteSet.Spec.ControllerEndpoint,
+		&satelliteSet.Spec.LinstorClientConfig,
+		lc.NamedSecret(ctx, r.client, satelliteSet.Spec.LinstorHttpsClientSecret),
+	)
 	if err != nil {
 		return []error{err}
 	}
 
-	err = r.controllerReachable(ctx, linstorClient)
-	if err != nil {
+	ok := linstorClient.ControllerReachable(ctx)
+	if !ok {
 		return []error{&reconcileutil.TemporaryError{
 			Source:       fmt.Errorf("failed to contact controller: %w", err),
 			RequeueAfter: connectionRetrySeconds * time.Second,
@@ -750,7 +737,11 @@ func (r *ReconcileLinstorSatelliteSet) reconcileLinstorStatus(ctx context.Contex
 		"Op":        "reconcileLinstorStatus",
 	})
 
-	linstorClient, err := r.getLinstorClient(ctx, satelliteSet)
+	linstorClient, err := lc.NewHighLevelLinstorClientFromConfig(
+		satelliteSet.Spec.ControllerEndpoint,
+		&satelliteSet.Spec.LinstorClientConfig,
+		lc.NamedSecret(ctx, r.client, satelliteSet.Spec.LinstorHttpsClientSecret),
+	)
 	if err != nil {
 		return err
 	}
@@ -758,8 +749,8 @@ func (r *ReconcileLinstorSatelliteSet) reconcileLinstorStatus(ctx context.Contex
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	err = r.controllerReachable(ctx, linstorClient)
-	if err != nil {
+	ok := linstorClient.ControllerReachable(ctx)
+	if !ok {
 		return fmt.Errorf("controller not reachable: %w", err)
 	}
 
@@ -1324,7 +1315,11 @@ func (r *ReconcileLinstorSatelliteSet) finalizeSatelliteSet(ctx context.Context,
 	errs := make([]error, 0)
 	keepNodes := make([]*shared.SatelliteStatus, 0)
 
-	linstorClient, err := r.getLinstorClient(ctx, satelliteSet)
+	linstorClient, err := lc.NewHighLevelLinstorClientFromConfig(
+		satelliteSet.Spec.ControllerEndpoint,
+		&satelliteSet.Spec.LinstorClientConfig,
+		lc.NamedSecret(ctx, r.client, satelliteSet.Spec.LinstorHttpsClientSecret),
+	)
 	if err != nil {
 		return []error{err}
 	}

pkg/controller/linstorcontroller/linstorcontroller_controller.go
@@ -32,7 +32,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
@@ -243,7 +242,11 @@ func (r *ReconcileLinstorController) reconcileControllers(ctx context.Context, c
 	})
 	log.Info("controller Reconcile: reconciling controller Nodes")
 
-	linstorClient, err := r.getLinstorClient(ctx, controllerResource)
+	linstorClient, err := lc.NewHighLevelLinstorClientFromConfig(
+		expectedEndpoint(controllerResource),
+		&controllerResource.Spec.LinstorClientConfig,
+		lc.NamedSecret(ctx, r.client, controllerResource.Namespace),
+	)
 	if err != nil {
 		return err
 	}
@@ -295,8 +298,7 @@ func (r *ReconcileLinstorController) reconcileControllers(ctx context.Context, c
 
 	meta := getObjectMeta(controllerResource, "%s-controller")
 	ourPods := &corev1.PodList{}
-	labelSelector := labels.SelectorFromSet(meta.Labels)
-	err = r.client.List(ctx, ourPods, client.InNamespace(controllerResource.Namespace), client.MatchingLabelsSelector{Selector: labelSelector})
+	err = r.client.List(ctx, ourPods, client.InNamespace(controllerResource.Namespace), client.MatchingLabels(meta.Labels))
 	if err != nil {
 		return err
 	}
@@ -376,7 +378,11 @@ func (r *ReconcileLinstorController) reconcileLinstorStatus(ctx context.Context,
 		"Op":        "reconcileLinstorStatus",
 	})
 
-	linstorClient, err := r.getLinstorClient(ctx, controllerResource)
+	linstorClient, err := lc.NewHighLevelLinstorClientFromConfig(
+		expectedEndpoint(controllerResource),
+		&controllerResource.Spec.LinstorClientConfig,
+		lc.NamedSecret(ctx, r.client, controllerResource.Namespace),
+	)
 	if err != nil {
 		return err
 	}
@@ -461,24 +467,6 @@ func (r *ReconcileLinstorController) reconcileLinstorStatus(ctx context.Context,
 	return nil
 }
 
-func (r *ReconcileLinstorController) getLinstorClient(ctx context.Context, controllerResource *piraeusv1.LinstorController) (*lc.HighLevelClient, error) {
-	log.Debug("get linstor client")
-
-	getSecret := func(secretName string) (map[string][]byte, error) {
-		secret := corev1.Secret{}
-
-		err := r.client.Get(ctx, types.NamespacedName{Name: secretName, Namespace: controllerResource.Namespace}, &secret)
-		if err != nil {
-			return nil, err
-		}
-
-		return secret.Data, nil
-	}
-
-	endpoint := expectedEndpoint(controllerResource)
-	return lc.NewHighLevelLinstorClientFromConfig(endpoint, &controllerResource.Spec.LinstorClientConfig, getSecret)
-}
-
 func (r *ReconcileLinstorController) findActiveControllerPodName(ctx context.Context, linstorClient *lc.HighLevelClient) (string, error) {
 	allNodes, err := linstorClient.Nodes.GetAll(ctx)
 	if err != nil {
@@ -516,7 +504,11 @@ func (r *ReconcileLinstorController) finalizeControllerSet(ctx context.Context, 
 		return nil
 	}
 
-	linstorClient, err := r.getLinstorClient(ctx, controllerResource)
+	linstorClient, err := lc.NewHighLevelLinstorClientFromConfig(
+		expectedEndpoint(controllerResource),
+		&controllerResource.Spec.LinstorClientConfig,
+		lc.NamedSecret(ctx, r.client, controllerResource.Namespace),
+	)
 	if err != nil {
 		return err
 	}
@@ -610,10 +602,11 @@ func newDeploymentForResource(controllerResource *piraeusv1.LinstorController) *
 		pullSecrets = append(pullSecrets, corev1.LocalObjectReference{Name: controllerResource.Spec.DrbdRepoCred})
 	}
 
-	const healthzPort = 9999
-	port := lc.DefaultHttpPort
+	healthzPort := 9999
+	port := lc.DefaultHTTPPort
+
 	if controllerResource.Spec.LinstorHttpsControllerSecret != "" {
-		port = lc.DefaultHttpsPort
+		port = lc.DefaultHTTPSPort
 	}
 
 	servicePorts := []corev1.EndpointPort{
@@ -831,11 +824,11 @@ func newDeploymentForResource(controllerResource *piraeusv1.LinstorController) *
 									Protocol:      "TCP",
 								},
 								{
-									ContainerPort: lc.DefaultHttpPort,
+									ContainerPort: lc.DefaultHTTPPort,
 									Protocol:      "TCP",
 								},
 								{
-									ContainerPort: lc.DefaultHttpsPort,
+									ContainerPort: lc.DefaultHTTPSPort,
 									Protocol:      "TCP",
 								},
 							},
@@ -879,9 +872,9 @@ func getDeploymentAffinity(controllerResource *piraeusv1.LinstorController) *cor
 }
 
 func newServiceForResource(controllerResource *piraeusv1.LinstorController) *corev1.Service {
-	port := lc.DefaultHttpPort
+	port := lc.DefaultHTTPPort
 	if controllerResource.Spec.LinstorHttpsControllerSecret != "" {
-		port = lc.DefaultHttpsPort
+		port = lc.DefaultHTTPSPort
 	}
 
 	return &corev1.Service{

pkg/linstor/client/client.go
@@ -9,27 +9,29 @@ import (
 	"net/url"
 	"os"
 	"strings"
-
-	"github.com/piraeusdatastore/piraeus-operator/pkg/apis/piraeus/shared"
+	"time"
 
 	linstor "github.com/LINBIT/golinstor"
 	lapi "github.com/LINBIT/golinstor/client"
-	kubeSpec "github.com/piraeusdatastore/piraeus-operator/pkg/k8s/spec"
 	"github.com/sirupsen/logrus"
+	ini "gopkg.in/ini.v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	ini "gopkg.in/ini.v1"
+	"github.com/piraeusdatastore/piraeus-operator/pkg/apis/piraeus/shared"
+	kubeSpec "github.com/piraeusdatastore/piraeus-operator/pkg/k8s/spec"
 )
 
 // Various lapi consts yet to be defined in golinstor.
 const (
-	Controller       = "CONTROLLER"
-	Satellite        = "SATELLITE"
-	Online           = "ONLINE"
-	Offline          = "OFFLINE"
-	DefaultHttpPort  = 3370
-	DefaultHttpsPort = 3371
+	Controller                 = "CONTROLLER"
+	Satellite                  = "SATELLITE"
+	Online                     = "ONLINE"
+	Offline                    = "OFFLINE"
+	DefaultHTTPPort            = 3370
+	DefaultHTTPSPort           = 3371
+	ControllerReachableTimeout = 10 * time.Second
 )
 
 // Global linstor client configuration, like controllers and connection settings
@@ -61,6 +63,20 @@ type HighLevelClient struct {
 }
 
 type SecretFetcher func(string) (map[string][]byte, error)
+
+// NamedSecret returns a SecretFetcher for the named secret.
+func NamedSecret(ctx context.Context, client client.Client, namespace string) SecretFetcher {
+	return func(secretName string) (map[string][]byte, error) {
+		secret := corev1.Secret{}
+
+		err := client.Get(ctx, types.NamespacedName{Name: secretName, Namespace: namespace}, &secret)
+		if err != nil {
+			return nil, fmt.Errorf("failed to fetch secret: %w", err)
+		}
+
+		return secret.Data, nil
+	}
+}
 
 // NewHighLevelLinstorClientFromConfig configures a HighLevelClient with an
 // in-cluster url based on service naming convention.
@@ -243,6 +259,16 @@ func (c *HighLevelClient) GetAllResourcesOnNode(ctx context.Context, nodeName st
 	return filterNodes(resList, nodeName), nil
 }
 
+// ControllerReachable returns true if the LINSTOR controller is up.
+func (c *HighLevelClient) ControllerReachable(ctx context.Context) bool {
+	ctx, cancel := context.WithTimeout(ctx, ControllerReachableTimeout)
+	defer cancel()
+
+	_, err := c.Controller.GetVersion(ctx)
+
+	return err == nil
+}
+
 func filterNodes(resources []lapi.ResourceWithVolumes, nodeName string) []lapi.ResourceWithVolumes {
 	nodeRes := make([]lapi.ResourceWithVolumes, 0)
 	for i := range resources {
@@ -310,9 +336,9 @@ func NewClientConfigForAPIResource(endpoint string, resource *shared.LinstorClie
 
 func DefaultControllerServiceEndpoint(serviceName types.NamespacedName, useHTTPS bool) string {
 	if useHTTPS {
-		return fmt.Sprintf("https://%s.%s.svc:%d", serviceName.Name, serviceName.Namespace, DefaultHttpsPort)
+		return fmt.Sprintf("https://%s.%s.svc:%d", serviceName.Name, serviceName.Namespace, DefaultHTTPSPort)
 	} else {
-		return fmt.Sprintf("http://%s.%s.svc:%d", serviceName.Name, serviceName.Namespace, DefaultHttpPort)
+		return fmt.Sprintf("http://%s.%s.svc:%d", serviceName.Name, serviceName.Namespace, DefaultHTTPPort)
 	}
 }
 

commit 44c99a988c1fddc72283d884186e6735079a78e1

Fix tls secret fetcher for satellite operator
2f0765a5a95 introduced a bug were the TLS secret to access LINSTOR was
requested from the wrong namespace. This commit fixes this issue by
passing the correct parameters to the NamedSecret function.

Reported-by: @OleksandrShtepa
Fixes: https://github.com/piraeusdatastore/piraeus-operator/issues/248
Signed-off-by: Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>

pkg/controller/linstorsatelliteset/linstorsatelliteset_controller.go
@@ -383,7 +383,7 @@ func (r *ReconcileLinstorSatelliteSet) reconcileAllNodesOnController(ctx context
 	linstorClient, err := lc.NewHighLevelLinstorClientFromConfig(
 		satelliteSet.Spec.ControllerEndpoint,
 		&satelliteSet.Spec.LinstorClientConfig,
-		lc.NamedSecret(ctx, r.client, satelliteSet.Spec.LinstorHttpsClientSecret),
+		lc.NamedSecret(ctx, r.client, satelliteSet.Namespace),
 	)
 	if err != nil {
 		return []error{err}
@@ -740,7 +740,7 @@ func (r *ReconcileLinstorSatelliteSet) reconcileLinstorStatus(ctx context.Contex
 	linstorClient, err := lc.NewHighLevelLinstorClientFromConfig(
 		satelliteSet.Spec.ControllerEndpoint,
 		&satelliteSet.Spec.LinstorClientConfig,
-		lc.NamedSecret(ctx, r.client, satelliteSet.Spec.LinstorHttpsClientSecret),
+		lc.NamedSecret(ctx, r.client, satelliteSet.Namespace),
 	)
 	if err != nil {
 		return err
@@ -1318,7 +1318,7 @@ func (r *ReconcileLinstorSatelliteSet) finalizeSatelliteSet(ctx context.Context,
 	linstorClient, err := lc.NewHighLevelLinstorClientFromConfig(
 		satelliteSet.Spec.ControllerEndpoint,
 		&satelliteSet.Spec.LinstorClientConfig,
-		lc.NamedSecret(ctx, r.client, satelliteSet.Spec.LinstorHttpsClientSecret),
+		lc.NamedSecret(ctx, r.client, satelliteSet.Namespace),
 	)
 	if err != nil {
 		return []error{err}