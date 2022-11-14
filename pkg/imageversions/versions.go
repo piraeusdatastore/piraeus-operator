415e0fd6be660e90bb2ca27caa769ea16ce87c63
Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>
2022-11-14 16:36:24 +0100

    Add default image configuration
    Adds a subpackages to create kustomize overrides for container images,
    based on a simple file-based default configuration.
    
    The configuration sets a specific repository base, and configures the actual
    image names + tags for all components in the pkg/resources package (with
    the exception of upstream k8s components).
    
    It also supports passing in the ".status.nodeInfo.osImage" information from
    the k8s node object. This field seems to be populated by /etc/os-release's
    PRETTY_NAME, which we can use to automatically select the right DRBD image
    to load.
    
    Signed-off-by: Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>

---
+++ pkg/imageversions/versions.go
@@ -0,0 +1,89 @@
+package imageversions
+
+import (
+	_ "embed"
+	"fmt"
+	"regexp"
+
+	kusttypes "sigs.k8s.io/kustomize/api/types"
+)
+
+// Config represents a default image mapping used by the operator.
+type Config struct {
+	Base         string                        `yaml:"base"`
+	Componenents map[Component]ComponentConfig `yaml:"componenents"`
+}
+
+type ComponentConfig struct {
+	Tag   string    `yaml:"tag"`
+	Match []OsMatch `yaml:"match"`
+	Image string    `yaml:"image"`
+}
+
+type OsMatch struct {
+	OsImage string `yaml:"osImage"`
+	Image   string `yaml:"image"`
+}
+
+type Component string
+
+const (
+	LinstorController Component = "linstor-controller"
+	LinstorSatellite  Component = "linstor-satellite"
+	LinstorCSI        Component = "linstor-csi"
+	DrbdReactor       Component = "drbd-reactor"
+	DrbdModuleLoader  Component = "drbd-module-loader"
+)
+
+type notConfigured struct {
+	c Component
+}
+
+func (n *notConfigured) Error() string {
+	return fmt.Sprintf("missing configuration for component '%s'", n.c)
+}
+
+var _ error = &notConfigured{}
+
+func (f *Config) GetVersions(base string, osImage string) ([]kusttypes.Image, error) {
+	result := make([]kusttypes.Image, 0, len(f.Componenents))
+	for c := range f.Componenents {
+		name, tag, err := f.get(c, base, osImage)
+		if err != nil {
+			return nil, err
+		}
+
+		if name != "" {
+			result = append(result, kusttypes.Image{
+				Name:    string(c),
+				NewName: name,
+				NewTag:  tag,
+			})
+		}
+	}
+
+	return result, nil
+}
+
+func (f *Config) get(c Component, base string, osImage string) (string, string, error) {
+	if base == "" {
+		base = f.Base
+	}
+
+	img, ok := f.Componenents[c]
+	if !ok {
+		return "", "", &notConfigured{c: c}
+	}
+
+	for _, matchRule := range img.Match {
+		if ok, _ := regexp.MatchString(matchRule.OsImage, osImage); ok {
+			return fmt.Sprintf("%s/%s", base, matchRule.Image), img.Tag, nil
+		}
+	}
+
+	if img.Image == "" {
+		return "", "", nil
+	}
+
+	return fmt.Sprintf("%s/%s", base, img.Image), img.Tag, nil
+}
---
+++ pkg/imageversions/versions_test.go
@@ -0,0 +1,78 @@
+package imageversions_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	kusttypes "sigs.k8s.io/kustomize/api/types"
+
+	"github.com/piraeusdatastore/piraeus-operator/v2/pkg/imageversions"
+)
+
+func TestConfigs_GetVersions(t *testing.T) {
+	t.Parallel()
+
+	base := imageversions.Config{
+		Base: "repo.example.com/base",
+		Componenents: map[imageversions.Component]imageversions.ComponentConfig{
+			imageversions.LinstorSatellite: {
+				Image: "satellite",
+				Tag:   "v1",
+			},
+			imageversions.DrbdModuleLoader: {
+				Tag:   "v2",
+				Image: "fallback",
+				Match: []imageversions.OsMatch{
+					{OsImage: "Ubuntu", Image: "ubuntu"},
+					{OsImage: "AlmaLinux [7-8]", Image: "old-alma"},
+					{OsImage: "AlmaLinux 9", Image: "new-alma"},
+				},
+			},
+		},
+	}
+
+	testcases := []struct {
+		name     string
+		base     string
+		os       string
+		expected []kusttypes.Image
+	}{
+		{
+			name: "default-ubuntu",
+			os:   "Ubuntu 20.04.5 LTS",
+			expected: []kusttypes.Image{
+				{Name: string(imageversions.LinstorSatellite), NewName: "repo.example.com/base/satellite", NewTag: "v1"},
+				{Name: string(imageversions.DrbdModuleLoader), NewName: "repo.example.com/base/ubuntu", NewTag: "v2"},
+			},
+		},
+		{
+			name: "with-base-new-alma",
+			base: "quay.io/example",
+			os:   "AlmaLinux 9.0 (Emerald Puma)",
+			expected: []kusttypes.Image{
+				{Name: string(imageversions.LinstorSatellite), NewName: "quay.io/example/satellite", NewTag: "v1"},
+				{Name: string(imageversions.DrbdModuleLoader), NewName: "quay.io/example/new-alma", NewTag: "v2"},
+			},
+		},
+		{
+			name: "with-base-fallback",
+			base: "quay.io/example2",
+			os:   "Debian GNU/Linux 11 (bullseye)",
+			expected: []kusttypes.Image{
+				{Name: string(imageversions.LinstorSatellite), NewName: "quay.io/example2/satellite", NewTag: "v1"},
+				{Name: string(imageversions.DrbdModuleLoader), NewName: "quay.io/example2/fallback", NewTag: "v2"},
+			},
+		},
+	}
+
+	for i := range testcases {
+		tcase := &testcases[i]
+		t.Run(tcase.name, func(t *testing.T) {
+			t.Parallel()
+
+			actual, err := base.GetVersions(tcase.base, tcase.os)
+			assert.NoError(t, err)
+			assert.ElementsMatch(t, tcase.expected, actual)
+		})
+	}
+}
