8426b0087db526e66cb9fd15a6b9cd70d907dc58
Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>
2022-11-22 14:26:54 +0100

    satellite-configuration: add merge helper
    LinstorSatelliteConfigurations apply to one or more satellites. We need to
    define a merge strategy, so that a set of Configurations result in a
    deterministic and sensible LinstorSatellite resource.
    
    This commit adds a helper method that merges a set of LSConfigurations into
    a single LSConfiguration. How the merge happens depends on the field:
    * Patches are concatinated, in the original order of configs. The assumption
      being that later patches override earlier patches if required.
    * StoragePools are merged by name. If a "later" config has a storge pool
      that shares the name with a storage pool specified earlier, the later
      configuration overrides the first.
    * Properties are merged by name. Like with StoragePools, later configurations
      override earlier versions.
    
    This means that the order of LSConfigurations supplied to the helper function
    is import. It is the responsibility of the caller to supply the configurations
    in a consistent and sensible order.
    
    Signed-off-by: Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>

---
+++ pkg/merge/linstorsatelliteconfiguration.go
@@ -0,0 +1,68 @@
+package merge
+
+import (
+	"sort"
+
+	piraeusv1 "github.com/piraeusdatastore/piraeus-operator/v2/api/v1"
+)
+
+// SatelliteConfigurations merges all configurations that apply based on the given node labels
+//
+// Merging happens by:
+// * Concatenating all patches in the matching configs
+// * Merging all properties by name. A property defined in a "later" config overrides previous property definitions.
+// * Merging all storage pools by name. A storage pool defined in a "later" config overrides previous property definitions.
+func SatelliteConfigurations(nodeLabels map[string]string, configs ...piraeusv1.LinstorSatelliteConfiguration) *piraeusv1.LinstorSatelliteConfiguration {
+	result := &piraeusv1.LinstorSatelliteConfiguration{}
+
+	propsMap := make(map[string]*piraeusv1.LinstorNodeProperty)
+	storPoolMap := make(map[string]*piraeusv1.LinstorStoragePool)
+
+	for i := range configs {
+		cfg := &configs[i]
+
+		if !SubsetOf(cfg.Spec.NodeSelector, nodeLabels) {
+			continue
+		}
+
+		for j := range cfg.Spec.Properties {
+			propsMap[cfg.Spec.Properties[j].Name] = &cfg.Spec.Properties[j]
+		}
+
+		for j := range cfg.Spec.StoragePools {
+			storPoolMap[cfg.Spec.StoragePools[j].Name] = &cfg.Spec.StoragePools[j]
+		}
+
+		result.Spec.Patches = append(result.Spec.Patches, cfg.Spec.Patches...)
+	}
+
+	for _, v := range propsMap {
+		result.Spec.Properties = append(result.Spec.Properties, *v)
+	}
+
+	sort.Slice(result.Spec.Properties, func(i, j int) bool {
+		return result.Spec.Properties[i].Name < result.Spec.Properties[j].Name
+	})
+
+	for _, v := range storPoolMap {
+		result.Spec.StoragePools = append(result.Spec.StoragePools, *v)
+	}
+
+	sort.Slice(result.Spec.StoragePools, func(i, j int) bool {
+		return result.Spec.StoragePools[i].Name < result.Spec.StoragePools[j].Name
+	})
+
+	return result
+}
+
+// SubsetOf returns true if all key and values in sub also appear in super.
+func SubsetOf(sub, super map[string]string) bool {
+	for k, subv := range sub {
+		superv, ok := super[k]
+		if !ok || superv != subv {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ pkg/merge/linstorsatelliteconfiguration_test.go
@@ -0,0 +1,141 @@
+package merge_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	piraeusv1 "github.com/piraeusdatastore/piraeus-operator/v2/api/v1"
+	"github.com/piraeusdatastore/piraeus-operator/v2/pkg/merge"
+)
+
+var (
+	Config1 = piraeusv1.LinstorSatelliteConfiguration{
+		Spec: piraeusv1.LinstorSatelliteConfigurationSpec{
+			NodeSelector: map[string]string{
+				"config1": "true",
+			},
+			Patches: []piraeusv1.Patch{
+				{Patch: "patch1"},
+				{Patch: "patch2"},
+			},
+			StoragePools: []piraeusv1.LinstorStoragePool{
+				{Name: "sp1", Lvm: &piraeusv1.LinstorStoragePoolLvm{VolumeGroup: "vg1"}, Source: &piraeusv1.LinstorStoragePoolSource{HostDevices: []string{"/dev/foobar"}}},
+				{Name: "sp2", Lvm: &piraeusv1.LinstorStoragePoolLvm{VolumeGroup: "vg2"}},
+			},
+			Properties: []piraeusv1.LinstorNodeProperty{
+				{Name: "prop1", Value: "config1"},
+				{Name: "prop2", Value: "config1"},
+			},
+		},
+	}
+	Config2 = piraeusv1.LinstorSatelliteConfiguration{
+		Spec: piraeusv1.LinstorSatelliteConfigurationSpec{
+			NodeSelector: map[string]string{
+				"config2": "true",
+			},
+			Patches: []piraeusv1.Patch{
+				{Patch: "patch3"},
+			},
+			StoragePools: []piraeusv1.LinstorStoragePool{
+				{Name: "sp1", LvmThin: &piraeusv1.LinstorStoragePoolLvmThin{VolumeGroup: "vg1", ThinPool: "thin1"}},
+			},
+			Properties: []piraeusv1.LinstorNodeProperty{
+				{Name: "prop1", Value: "config2"},
+				{Name: "prop3", Value: "config2"},
+			},
+		},
+	}
+	Config3 = piraeusv1.LinstorSatelliteConfiguration{
+		Spec: piraeusv1.LinstorSatelliteConfigurationSpec{
+			NodeSelector: map[string]string{
+				"config3": "true",
+			},
+			Patches: []piraeusv1.Patch{
+				{Patch: "patch4"},
+				{Patch: "patch5"},
+			},
+			StoragePools: []piraeusv1.LinstorStoragePool{
+				{Name: "sp2", LvmThin: &piraeusv1.LinstorStoragePoolLvmThin{VolumeGroup: "vg2", ThinPool: "thin2"}},
+				{Name: "sp3", Source: &piraeusv1.LinstorStoragePoolSource{HostDevices: []string{"/dev/bla"}}},
+			},
+			Properties: []piraeusv1.LinstorNodeProperty{
+				{Name: "prop2", Value: "config3"},
+				{Name: "prop3", Value: "config3"},
+			},
+		},
+	}
+)
+
+func TestMergeSatelliteConfigurations(t *testing.T) {
+	t.Parallel()
+
+	testcases := []struct {
+		name    string
+		labels  map[string]string
+		configs []piraeusv1.LinstorSatelliteConfiguration
+		result  *piraeusv1.LinstorSatelliteConfiguration
+	}{
+		{
+			name:   "empty",
+			result: &piraeusv1.LinstorSatelliteConfiguration{},
+		},
+		{
+			name:    "config-no-match",
+			configs: []piraeusv1.LinstorSatelliteConfiguration{Config1, Config2, Config3},
+			result:  &piraeusv1.LinstorSatelliteConfiguration{},
+		},
+		{
+			name: "merge-all",
+			labels: map[string]string{
+				"config1": "true",
+				"config2": "true",
+				"config3": "true",
+			},
+			configs: []piraeusv1.LinstorSatelliteConfiguration{Config1, Config2, Config3},
+			result: &piraeusv1.LinstorSatelliteConfiguration{
+				Spec: piraeusv1.LinstorSatelliteConfigurationSpec{
+					Patches: []piraeusv1.Patch{
+						{Patch: "patch1"},
+						{Patch: "patch2"},
+						{Patch: "patch3"},
+						{Patch: "patch4"},
+						{Patch: "patch5"},
+					},
+					StoragePools: []piraeusv1.LinstorStoragePool{
+						{Name: "sp1", LvmThin: &piraeusv1.LinstorStoragePoolLvmThin{VolumeGroup: "vg1", ThinPool: "thin1"}},
+						{Name: "sp2", LvmThin: &piraeusv1.LinstorStoragePoolLvmThin{VolumeGroup: "vg2", ThinPool: "thin2"}},
+						{Name: "sp3", Source: &piraeusv1.LinstorStoragePoolSource{HostDevices: []string{"/dev/bla"}}},
+					},
+					Properties: []piraeusv1.LinstorNodeProperty{
+						{Name: "prop1", Value: "config2"},
+						{Name: "prop2", Value: "config3"},
+						{Name: "prop3", Value: "config3"},
+					},
+				},
+			},
+		},
+		{
+			name:    "filter",
+			labels:  map[string]string{"config2": "true"},
+			configs: []piraeusv1.LinstorSatelliteConfiguration{Config1, Config2, Config3},
+			result: &piraeusv1.LinstorSatelliteConfiguration{
+				Spec: piraeusv1.LinstorSatelliteConfigurationSpec{
+					Patches:      Config2.Spec.Patches,
+					StoragePools: Config2.Spec.StoragePools,
+					Properties:   Config2.Spec.Properties,
+				},
+			},
+		},
+	}
+
+	for i := range testcases {
+		tcase := &testcases[i]
+		t.Run(tcase.name, func(t *testing.T) {
+			t.Parallel()
+
+			actual := merge.SatelliteConfigurations(tcase.labels, tcase.configs...)
+			assert.Equal(t, tcase.result, actual)
+		})
+	}
+}
