7a5405ec80a5c37b138436581d02922a4fbefeef
Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>
2022-11-23 12:25:22 +0100

    api: add LinstorCluster resources
    A LinstorCluster resource is the single entity controlling creation of the
    whole LINSTOR cluster + CSI driver. From it, the operator will create
    the expected LinstorSatellite resources, as well as the LINSTOR Controller,
    CSI Controller and CSI Node DaemonSet.
    
    The resource itself allows for:
    * Changing the default repository used to pull images.
    * Deploying the whole storage stack on only a subset of nodes.
    * Setting cluster wide LINSTOR properties (== at controller level).
    * Applying kustomize patches to any created k8s resource.
    
    Signed-off-by: Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>

---
+++ api/v1/linstorcluster_types.go
@@ -0,0 +1,83 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// LinstorClusterSpec defines the desired state of LinstorCluster
+type LinstorClusterSpec struct {
+	// Repository used to pull workload images.
+	// +kubebuilder:validation:Optional
+	Repository string `json:"repository,omitempty"`
+
+	// NodeSelector selects the nodes on which LINSTOR Satellites will be deployed.
+	// See https://kubernetes.io/docs/concepts/configuration/assign-pod-node/
+	// +kubebuilder:validation:Optional
+	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
+
+	// Properties to apply on the cluster level.
+	//
+	// Use to create default settings for DRBD that should apply to all resources or to configure some other cluster
+	// wide default.
+	// +kubebuilder:validation:Optional
+	// +listType=map
+	// +listMapKey=name
+	// +patchMergeKey=name
+	// +patchStrategy=merge
+	Properties []LinstorControllerProperty `json:"properties,omitempty"`
+
+	// Patches is a list of kustomize patches to apply.
+	//
+	// See https://kubectl.docs.kubernetes.io/references/kustomize/kustomization/patches/ for how to create patches.
+	// +kubebuilder:validation:Optional
+	Patches []Patch `json:"patches,omitempty"`
+}
+
+// LinstorClusterStatus defines the observed state of LinstorCluster
+type LinstorClusterStatus struct {
+	// Current LINSTOR Cluster state
+	// +kubebuilder:validation:Optional
+	// +listType=map
+	// +listMapKey=type
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+}
+
+// LinstorCluster is the Schema for the linstorclusters API
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster
+type LinstorCluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   LinstorClusterSpec   `json:"spec,omitempty"`
+	Status LinstorClusterStatus `json:"status,omitempty"`
+}
+
+// LinstorClusterList contains a list of LinstorCluster
+// +kubebuilder:object:root=true
+type LinstorClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []LinstorCluster `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&LinstorCluster{}, &LinstorClusterList{})
+}
---
+++ api/v1/linstorcluster_webhook_test.go
@@ -0,0 +1,107 @@
+package v1_test
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	piraeusv1 "github.com/piraeusdatastore/piraeus-operator/v2/api/v1"
+)
+
+var _ = Describe("LinstorCluster webhook", func() {
+	typeMeta := metav1.TypeMeta{
+		Kind:       "LinstorCluster",
+		APIVersion: piraeusv1.GroupVersion.String(),
+	}
+	complexCluster := &piraeusv1.LinstorCluster{
+		TypeMeta:   typeMeta,
+		ObjectMeta: metav1.ObjectMeta{Name: "cluster1"},
+		Spec: piraeusv1.LinstorClusterSpec{
+			NodeSelector: map[string]string{
+				"node-role.kubernetes.io/with-storage": "",
+			},
+			Properties: []piraeusv1.LinstorControllerProperty{
+				{Name: "PrefNic", Value: "default-ipv6"},
+				{Name: "Aux/foo", Value: "bar"},
+			},
+			Patches: []piraeusv1.Patch{
+				{
+					Target: &piraeusv1.Selector{
+						Name: "csi-controller",
+						Kind: "Deployment",
+					},
+					Patch: "[{\"op\": \"replace\", \"path\": \"/metadata/name\", \"value\": \"csi-controller-of-the-universe\"}]",
+				},
+			},
+		},
+	}
+
+	AfterEach(func(ctx context.Context) {
+		err := k8sClient.DeleteAllOf(ctx, &piraeusv1.LinstorCluster{})
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should allow empty cluster", func(ctx context.Context) {
+		cluster := &piraeusv1.LinstorCluster{TypeMeta: typeMeta, ObjectMeta: metav1.ObjectMeta{Name: "cluster1"}}
+		err := k8sClient.Patch(ctx, cluster, client.Apply, client.FieldOwner("test"), client.ForceOwnership)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should allow complex cluster", func(ctx context.Context) {
+		err := k8sClient.Patch(ctx, complexCluster.DeepCopy(), client.Apply, client.FieldOwner("test"), client.ForceOwnership)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should allow updating a complex cluster", func(ctx context.Context) {
+		err := k8sClient.Patch(ctx, complexCluster.DeepCopy(), client.Apply, client.FieldOwner("test"), client.ForceOwnership)
+		Expect(err).NotTo(HaveOccurred())
+
+		clusterCopy := complexCluster.DeepCopy()
+		clusterCopy.Spec.Patches[0].Patch = "apiVersion: apps/v1\nkind: Deployment\nmetadata:\n  name: linstor-controller\n  annotations:\n    k8s.v1.cni.cncf.io/networks: eth1\nspec:\n  containers:\n  - name: linstor-satellite\n    volumeMounts:\n    - name: var-lib-drbd\n      mountPath: /var/lib/drbd\n  volumes:\n  - name: var-lib-drbd\n    emptyDir: {}\n"
+		err = k8sClient.Patch(ctx, clusterCopy, client.Apply, client.FieldOwner("test"), client.ForceOwnership)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should reject improper node selectors", func(ctx context.Context) {
+		clusterConfig := &piraeusv1.LinstorCluster{
+			TypeMeta:   typeMeta,
+			ObjectMeta: metav1.ObjectMeta{Name: "invalid-labels"},
+			Spec: piraeusv1.LinstorClusterSpec{
+				NodeSelector: map[string]string{
+					"example.com/key1":           "valid-label",
+					"12.34.not+a+valid+key/key1": "valid-value",
+					"example.com/key2":           "not a valid value",
+				},
+			},
+		}
+		err := k8sClient.Patch(ctx, clusterConfig, client.Apply, client.FieldOwner("test"), client.ForceOwnership)
+		Expect(err).To(HaveOccurred())
+		statusErr := err.(*errors.StatusError)
+		Expect(statusErr).NotTo(BeNil())
+		Expect(statusErr.ErrStatus.Details).NotTo(BeNil())
+		Expect(statusErr.ErrStatus.Details.Causes).To(HaveLen(2))
+	})
+
+	It("should reject improper patches", func(ctx context.Context) {
+		clusterConfig := &piraeusv1.LinstorCluster{
+			TypeMeta:   typeMeta,
+			ObjectMeta: metav1.ObjectMeta{Name: "invalid-labels"},
+			Spec: piraeusv1.LinstorClusterSpec{
+				Patches: []piraeusv1.Patch{
+					{Patch: "# just some random yaml, not a partial k8s resource\nroses: red\nviolets: blue\n"},
+					{Patch: "not even any structure at all\njust some random text"},
+				},
+			},
+		}
+		err := k8sClient.Patch(ctx, clusterConfig, client.Apply, client.FieldOwner("test"), client.ForceOwnership)
+		Expect(err).To(HaveOccurred())
+		statusErr := err.(*errors.StatusError)
+		Expect(statusErr).NotTo(BeNil())
+		Expect(statusErr.ErrStatus.Details).NotTo(BeNil())
+		Expect(statusErr.ErrStatus.Details.Causes).To(HaveLen(2))
+	})
+})
