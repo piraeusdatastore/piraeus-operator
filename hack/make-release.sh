2087b9f0b84df69721b27699bfbf9d4a02b607bd
Moritz Wanzenböck <moritz.wanzenboeck@linbit.com>
2023-12-05 09:28:38 +0100

    hack: keep latest release in instructions
    Oftentimes users just deploy from the front page, which happens
    to point to the generic "v2" instructions. This often leads to users
    being surprised when the image gets updated and their cluster is no
    longer working smoothly.
    
    Instead of showing the generic "v2" instructions, just leave the instructions
    from the last release.
    
    Signed-off-by: Moritz Wanzenböck <moritz.wanzenboeck@linbit.com>

--- hack/make-release.sh
+++ hack/make-release.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 YQ="${YQ:-yq}"
 KUSTOMIZE="${KUSTOMIZE:-kustomize}"
@@ -41,31 +41,28 @@ popd
 
 # replace deployment instructions in docs
 for FILE in ./README.md ./docs/tutorial/get-started.md ; do
-	sed -e "s/ref=v2/ref=v$VERSION/" -i "$FILE"
+	sed -e "s/ref=v[0-9\.]\+/ref=v$VERSION/" -i "$FILE"
 done
+
+# replace version in Makefile
+sed -e "s/^VERSION ?=.*/VERSION ?= $VERSION/" -i Makefile
 
 # replace chart version+appVersion
 $YQ ".version = \"$VERSION\"" -i charts/piraeus/Chart.yaml
 $YQ ".appVersion = \"v$VERSION\"" -i charts/piraeus/Chart.yaml
 
-# commit as current release + tag
+# commit as current release
+# We don't do git tag v$(VERSION) here, as the commit will change once its merged in github
 git commit -aevm "Release v$VERSION" --signoff
 
-# We don't do git tag v$(VERSION) here, as the commit will change once its merged in github
 # add "Unreleased" section at top + create comparison link against current master
 sed "s/^## \[v$VERSION\]/## [Unreleased]\n\n## [v$VERSION]/" -i CHANGELOG.md
 echo "[Unreleased]: https://github.com/piraeusdatastore/piraeus-operator/compare/v$VERSION...HEAD" >> CHANGELOG.md
 
+# Reset image version for kustomize
 pushd config/default
 $KUSTOMIZE edit set image controller="$IMG:v2"
 popd
-
-$YQ ".version = \"$VERSION-dev\"" -i charts/piraeus/Chart.yaml
-$YQ ".appVersion = \"v2\"" -i charts/piraeus/Chart.yaml
-
-for FILE in ./README.md ./docs/tutorial/get-started.md ; do
-	sed -e "s/ref=v$VERSION/ref=v2/" -i "$FILE"
-done
 
 # commit begin of new dev cycle
 git commit -aevm "Prepare next dev cycle" --signoff
