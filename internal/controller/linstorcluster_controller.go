piraeusdatastore/piraeus-operator

commit 534ae5b7db5c30e3f6fca77a73f074328243a4f4

Report additional status for LinstorSatelliteConfiguration
* Node selector
* Applied status
* Number of matched satellites
* Name of applied satellites

Signed-off-by: Moritz Wanzenböck <moritz.wanzenboeck@linbit.com>

internal/controller/linstorcluster_controller.go
@@ -27,6 +27,7 @@ import (
 
 	lapi "github.com/LINBIT/golinstor/client"
 	certmanagerv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	"github.com/go-openapi/jsonpointer"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
@@ -185,7 +186,7 @@ func (r *LinstorClusterReconciler) reconcileAppliedResource(ctx context.Context,
 		return err
 	}
 
-	resMap, err := r.kustomizeResources(ctx, lcluster, satelliteNodes.Items, satelliteConfigs.Items, existingSatellites.Items)
+	resMap, appliedConfigurations, err := r.kustomizeResources(ctx, lcluster, satelliteNodes.Items, satelliteConfigs.Items, existingSatellites.Items)
 	if err != nil {
 		return err
 	}
@@ -221,6 +222,16 @@ func (r *LinstorClusterReconciler) reconcileAppliedResource(ctx context.Context,
 				ObservedGeneration: config.Generation,
 			})
 
+			for node, configs := range appliedConfigurations {
+				if slices.Contains(configs, config.Name) {
+					config.Status.AppliedTo = append(config.Status.AppliedTo, node)
+				}
+			}
+
+			slices.Sort(config.Status.AppliedTo)
+			matched := int64(len(config.Status.AppliedTo))
+			config.Status.Matched = &matched
+
 			return nil
 		})
 		if err != nil {
@@ -249,42 +260,45 @@ func (r *LinstorClusterReconciler) reconcileAppliedResource(ctx context.Context,
 	return nil
 }
 
-func (r *LinstorClusterReconciler) kustomizeResources(ctx context.Context, lcluster *piraeusiov1.LinstorCluster, satelliteNodes []corev1.Node, configs []piraeusiov1.LinstorSatelliteConfiguration, existingSatellites []piraeusiov1.LinstorSatellite) (resmap.ResMap, error) {
+// kustomizeResources builds the resources to apply to the cluster.
+//
+// In addition to the resources to apply, it returns the list of LinstorSatelliteConfigurations applied to each Satellite.
+func (r *LinstorClusterReconciler) kustomizeResources(ctx context.Context, lcluster *piraeusiov1.LinstorCluster, satelliteNodes []corev1.Node, configs []piraeusiov1.LinstorSatelliteConfiguration, existingSatellites []piraeusiov1.LinstorSatellite) (resmap.ResMap, map[string][]string, error) {
 	cfg, err := imageversions.FromConfigMap(ctx, r.Client, types.NamespacedName{Name: r.ImageConfigMapName, Namespace: r.Namespace})
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	imgs, _ := cfg.GetVersions(lcluster.Spec.Repository, "")
 
 	ctrlRes, err := r.kustomizeControllerResources(lcluster, imgs)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	csiControllerRes, err := r.kustomizeCSIControllerResources(lcluster, imgs)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	csiNodeRes, err := r.kustomizeCSINodeResources(lcluster, imgs)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	haControllerRes, err := r.kustomizeHAControllerResources(lcluster, imgs)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	affinityControllerRes, err := r.kustomizeAffinityControllerResources(lcluster, imgs)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	commonNodeRes, err := r.kustomizeNodeCommonResources(lcluster, imgs)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	resMap := resmap.New()
@@ -294,6 +308,8 @@ func (r *LinstorClusterReconciler) kustomizeResources(ctx context.Context, lclus
 	})
 
 	satelliteTolerations := tolerations.MergeTolerations(tolerations.DefaultDaemonSetTolerations, tolerations.HAControllerTolerations, lcluster.Spec.Tolerations)
+
+	appliedConfigurations := make(map[string][]string)
 
 	for i := range satelliteNodes {
 		node := &satelliteNodes[i]
@@ -320,48 +336,50 @@ func (r *LinstorClusterReconciler) kustomizeResources(ctx context.Context, lclus
 			continue
 		}
 
-		satRes, err := r.kustomizeLinstorSatellite(ctx, lcluster, node, existingSatellite, configs, imgs)
+		satRes, matched, err := r.kustomizeLinstorSatellite(ctx, lcluster, node, existingSatellite, configs, imgs)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 
 		err = resMap.AppendAll(satRes)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
+
+		appliedConfigurations[node.Name] = matched
 	}
 
 	err = resMap.AppendAll(ctrlRes)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	err = resMap.AppendAll(csiControllerRes)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	err = resMap.AppendAll(csiNodeRes)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	err = resMap.AppendAll(haControllerRes)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	err = resMap.AppendAll(affinityControllerRes)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	err = resMap.AppendAll(commonNodeRes)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	return resMap, nil
+	return resMap, appliedConfigurations, nil
 }
 
 // Create the LINSTOR Controller resources.
@@ -813,7 +831,9 @@ func (r *LinstorClusterReconciler) kustomizeNodeCommonResources(lcluster *piraeu
 // * Set the cluster reference to the owning LinstorCluster
 // * Apply the result of merging all LinstorSatelliteConfigurations to the LinstorSatellite
 // * user defined patches
-func (r *LinstorClusterReconciler) kustomizeLinstorSatellite(ctx context.Context, lcluster *piraeusiov1.LinstorCluster, node *corev1.Node, existingSatellite *piraeusiov1.LinstorSatellite, configs []piraeusiov1.LinstorSatelliteConfiguration, imgs []kusttypes.Image) (resmap.ResMap, error) {
+//
+// In addition to the resources, it returns the names of the LinstorSatelliteConfigurations applied to the Satellite.
+func (r *LinstorClusterReconciler) kustomizeLinstorSatellite(ctx context.Context, lcluster *piraeusiov1.LinstorCluster, node *corev1.Node, existingSatellite *piraeusiov1.LinstorSatellite, configs []piraeusiov1.LinstorSatelliteConfiguration, imgs []kusttypes.Image) (resmap.ResMap, []string, error) {
 	renamePatch := utils.JsonPatch{
 		Op:    utils.Replace,
 		Path:  "/metadata/name",
@@ -862,7 +882,7 @@ func (r *LinstorClusterReconciler) kustomizeLinstorSatellite(ctx context.Context
 	t := tolerations.MergeTolerations(tolerations.HAControllerTolerations, lcluster.Spec.Tolerations)
 	tolerationsPatches, err := TolerationsPatch("DaemonSet", "linstor-satellite", t)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	for _, p := range tolerationsPatches {
@@ -873,7 +893,13 @@ func (r *LinstorClusterReconciler) kustomizeLinstorSatellite(ctx context.Context
 		})
 	}
 
-	cfg := merge.SatelliteConfigurations(ctx, node, configs...)
+	cfg, matched := merge.SatelliteConfigurations(ctx, node, configs...)
+
+	patches = append(patches, utils.JsonPatch{
+		Op:    utils.Add,
+		Path:  "/metadata/annotations/" + jsonpointer.Escape(vars.AppliedConfigurationAnnotation),
+		Value: strings.Join(matched, ","),
+	})
 
 	if cfg.Spec.InternalTLS != nil {
 		patches = append(patches, utils.JsonPatch{
@@ -928,10 +954,11 @@ func (r *LinstorClusterReconciler) kustomizeLinstorSatellite(ctx context.Context
 		patches,
 	)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	return r.kustomize([]string{"satellite"}, lcluster, imgs, *patch)
+	res, err := r.kustomize([]string{"satellite"}, lcluster, imgs, *patch)
+	return res, matched, err
 }
 
 // kustomize applies the common Kustomizations along with the given patches.

api/v1/linstorsatelliteconfiguration_types.go
@@ -94,12 +94,25 @@ type LinstorSatelliteConfigurationStatus struct {
 	// +listType=map
 	// +listMapKey=type
 	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+
+	// AppliedTo lists the LinstorSatellite resource this configuration was applied to
+	// +kubebuilder:validation:Optional
+	AppliedTo []string `json:"appliedTo"`
+
+	// Number of configured LinstorSatellite resource.
+	// +kubebuilder:validation:Optional
+	Matched *int64 `json:"matched"`
 }
 
 // LinstorSatelliteConfiguration is the Schema for the linstorsatelliteconfigurations API
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:scope=Cluster
+// +kubebuilder:printcolumn:name="Selector",type=string,JSONPath=`.spec.nodeSelector`,description="The node selector used"
+// +kubebuilder:printcolumn:name="Applied",type=string,JSONPath=`.status.conditions[?(@.type=='Applied')].status`,description="If the Configuration was applied"
+// +kubebuilder:printcolumn:name="Matched",type=integer,JSONPath=`.status.matched`,description="Number of Satellites this Configuration has been applied to"
+// +kubebuilder:printcolumn:name="Satellites",type=string,JSONPath=`.status.appliedTo`,description="Satellites this Configuration has been applied to",priority=10
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 type LinstorSatelliteConfiguration struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`

api/v1/zz_generated.deepcopy.go
@@ -709,6 +709,16 @@ func (in *LinstorSatelliteConfigurationStatus) DeepCopyInto(out *LinstorSatellit
 			(*in)[i].DeepCopyInto(&(*out)[i])
 		}
 	}
+	if in.AppliedTo != nil {
+		in, out := &in.AppliedTo, &out.AppliedTo
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.Matched != nil {
+		in, out := &in.Matched, &out.Matched
+		*out = new(int64)
+		**out = **in
+	}
 }
 
 // DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new LinstorSatelliteConfigurationStatus.

charts/piraeus/templates/crds.yaml
@@ -883,7 +883,28 @@ spec:
     singular: linstorsatelliteconfiguration
   scope: Cluster
   versions:
-  - name: v1
+  - additionalPrinterColumns:
+    - description: The node selector used
+      jsonPath: .spec.nodeSelector
+      name: Selector
+      type: string
+    - description: If the Configuration was applied
+      jsonPath: .status.conditions[?(@.type=='Applied')].status
+      name: Applied
+      type: string
+    - description: Number of Satellites this Configuration has been applied to
+      jsonPath: .status.matched
+      name: Matched
+      type: integer
+    - description: Satellites this Configuration has been applied to
+      jsonPath: .status.appliedTo
+      name: Satellites
+      priority: 10
+      type: string
+    - jsonPath: .metadata.creationTimestamp
+      name: Age
+      type: date
+    name: v1
     schema:
       openAPIV3Schema:
         description: LinstorSatelliteConfiguration is the Schema for the linstorsatelliteconfigurations
@@ -1380,6 +1401,12 @@ spec:
             description: LinstorSatelliteConfigurationStatus defines the observed
               state of LinstorSatelliteConfiguration
             properties:
+              appliedTo:
+                description: AppliedTo lists the LinstorSatellite resource this configuration
+                  was applied to
+                items:
+                  type: string
+                type: array
               conditions:
                 description: Current LINSTOR Satellite Config state
                 items:
@@ -1440,6 +1467,10 @@ spec:
                 x-kubernetes-list-map-keys:
                 - type
                 x-kubernetes-list-type: map
+              matched:
+                description: Number of configured LinstorSatellite resource.
+                format: int64
+                type: integer
             type: object
         type: object
     served: true

config/crd/bases/piraeus.io_linstorsatelliteconfigurations.yaml
@@ -14,7 +14,28 @@ spec:
     singular: linstorsatelliteconfiguration
   scope: Cluster
   versions:
-  - name: v1
+  - additionalPrinterColumns:
+    - description: The node selector used
+      jsonPath: .spec.nodeSelector
+      name: Selector
+      type: string
+    - description: If the Configuration was applied
+      jsonPath: .status.conditions[?(@.type=='Applied')].status
+      name: Applied
+      type: string
+    - description: Number of Satellites this Configuration has been applied to
+      jsonPath: .status.matched
+      name: Matched
+      type: integer
+    - description: Satellites this Configuration has been applied to
+      jsonPath: .status.appliedTo
+      name: Satellites
+      priority: 10
+      type: string
+    - jsonPath: .metadata.creationTimestamp
+      name: Age
+      type: date
+    name: v1
     schema:
       openAPIV3Schema:
         description: LinstorSatelliteConfiguration is the Schema for the linstorsatelliteconfigurations
@@ -511,6 +532,12 @@ spec:
             description: LinstorSatelliteConfigurationStatus defines the observed
               state of LinstorSatelliteConfiguration
             properties:
+              appliedTo:
+                description: AppliedTo lists the LinstorSatellite resource this configuration
+                  was applied to
+                items:
+                  type: string
+                type: array
               conditions:
                 description: Current LINSTOR Satellite Config state
                 items:
@@ -571,6 +598,10 @@ spec:
                 x-kubernetes-list-map-keys:
                 - type
                 x-kubernetes-list-type: map
+              matched:
+                description: Number of configured LinstorSatellite resource.
+                format: int64
+                type: integer
             type: object
         type: object
     served: true

pkg/merge/linstorsatelliteconfiguration.go
@@ -19,8 +19,9 @@ import (
 // * Concatenating all patches in the matching configs
 // * Merging all properties by name. A property defined in a "later" config overrides previous property definitions.
 // * Merging all storage pools by name. A storage pool defined in a "later" config overrides previous property definitions.
-func SatelliteConfigurations(ctx context.Context, node *corev1.Node, configs ...piraeusv1.LinstorSatelliteConfiguration) *piraeusv1.LinstorSatelliteConfiguration {
+func SatelliteConfigurations(ctx context.Context, node *corev1.Node, configs ...piraeusv1.LinstorSatelliteConfiguration) (*piraeusv1.LinstorSatelliteConfiguration, []string) {
 	result := &piraeusv1.LinstorSatelliteConfiguration{}
+	var matched []string
 
 	propsMap := make(map[string]*piraeusv1.LinstorNodeProperty)
 	storPoolMap := make(map[string]*piraeusv1.LinstorStoragePool)
@@ -37,6 +38,8 @@ func SatelliteConfigurations(ctx context.Context, node *corev1.Node, configs ...
 				continue
 			}
 		}
+
+		matched = append(matched, cfg.Name)
 
 		for j := range cfg.Spec.Properties {
 			propsMap[cfg.Spec.Properties[j].Name] = &cfg.Spec.Properties[j]
@@ -84,7 +87,7 @@ func SatelliteConfigurations(ctx context.Context, node *corev1.Node, configs ...
 		return result.Spec.StoragePools[i].Name < result.Spec.StoragePools[j].Name
 	})
 
-	return result
+	return result, matched
 }
 
 type satellitePatch struct {

pkg/merge/linstorsatelliteconfiguration_test.go
@@ -15,6 +15,7 @@ import (
 
 var (
 	Config1 = piraeusv1.LinstorSatelliteConfiguration{
+		ObjectMeta: metav1.ObjectMeta{Name: "Config1"},
 		Spec: piraeusv1.LinstorSatelliteConfigurationSpec{
 			NodeSelector: map[string]string{
 				"config1": "true",
@@ -37,6 +38,7 @@ var (
 		},
 	}
 	Config2 = piraeusv1.LinstorSatelliteConfiguration{
+		ObjectMeta: metav1.ObjectMeta{Name: "Config2"},
 		Spec: piraeusv1.LinstorSatelliteConfigurationSpec{
 			NodeSelector: map[string]string{
 				"config2": "true",
@@ -54,6 +56,7 @@ var (
 		},
 	}
 	Config3 = piraeusv1.LinstorSatelliteConfiguration{
+		ObjectMeta: metav1.ObjectMeta{Name: "Config3"},
 		Spec: piraeusv1.LinstorSatelliteConfigurationSpec{
 			NodeSelector: map[string]string{
 				"config3": "true",
@@ -76,6 +79,7 @@ var (
 		},
 	}
 	Config4 = piraeusv1.LinstorSatelliteConfiguration{
+		ObjectMeta: metav1.ObjectMeta{Name: "Config4"},
 		Spec: piraeusv1.LinstorSatelliteConfigurationSpec{
 			NodeAffinity: &corev1.NodeSelector{
 				NodeSelectorTerms: []corev1.NodeSelectorTerm{
@@ -98,6 +102,7 @@ var (
 		},
 	}
 	Config5 = piraeusv1.LinstorSatelliteConfiguration{
+		ObjectMeta: metav1.ObjectMeta{Name: "Config5"},
 		Spec: piraeusv1.LinstorSatelliteConfigurationSpec{
 			PodTemplate: json.RawMessage(`{"spec": {"hostNetwork": true}}`),
 		},
@@ -112,6 +117,7 @@ func TestMergeSatelliteConfigurations(t *testing.T) {
 		labels  map[string]string
 		configs []piraeusv1.LinstorSatelliteConfiguration
 		result  *piraeusv1.LinstorSatelliteConfiguration
+		matched []string
 	}{
 		{
 			name:   "empty",
@@ -129,6 +135,7 @@ func TestMergeSatelliteConfigurations(t *testing.T) {
 				"config2": "true",
 				"config3": "true",
 			},
+			matched: []string{"Config1", "Config2", "Config3"},
 			configs: []piraeusv1.LinstorSatelliteConfiguration{Config1, Config2, Config3},
 			result: &piraeusv1.LinstorSatelliteConfiguration{
 				Spec: piraeusv1.LinstorSatelliteConfigurationSpec{
@@ -158,6 +165,7 @@ func TestMergeSatelliteConfigurations(t *testing.T) {
 		{
 			name:    "filter",
 			labels:  map[string]string{"config2": "true"},
+			matched: []string{"Config2"},
 			configs: []piraeusv1.LinstorSatelliteConfiguration{Config1, Config2, Config3},
 			result: &piraeusv1.LinstorSatelliteConfiguration{
 				Spec: piraeusv1.LinstorSatelliteConfigurationSpec{
@@ -176,6 +184,7 @@ func TestMergeSatelliteConfigurations(t *testing.T) {
 		{
 			name:    "complex-filter-positive",
 			labels:  map[string]string{"config4": "false"},
+			matched: []string{"Config4"},
 			configs: []piraeusv1.LinstorSatelliteConfiguration{Config4},
 			result: &piraeusv1.LinstorSatelliteConfiguration{
 				Spec: piraeusv1.LinstorSatelliteConfigurationSpec{
@@ -188,6 +197,7 @@ func TestMergeSatelliteConfigurations(t *testing.T) {
 		{
 			name:    "patch-convert",
 			configs: []piraeusv1.LinstorSatelliteConfiguration{Config5},
+			matched: []string{"Config5"},
 			result: &piraeusv1.LinstorSatelliteConfiguration{
 				Spec: piraeusv1.LinstorSatelliteConfigurationSpec{
 					Patches: []piraeusv1.Patch{{
@@ -204,11 +214,12 @@ func TestMergeSatelliteConfigurations(t *testing.T) {
 		t.Run(tcase.name, func(t *testing.T) {
 			t.Parallel()
 
-			actual := merge.SatelliteConfigurations(context.Background(), &corev1.Node{ObjectMeta: metav1.ObjectMeta{
+			actual, actualMatched := merge.SatelliteConfigurations(context.Background(), &corev1.Node{ObjectMeta: metav1.ObjectMeta{
 				Name:   tcase.name,
 				Labels: tcase.labels,
 			}}, tcase.configs...)
 			assert.Equal(t, tcase.result, actual)
+			assert.Equal(t, tcase.matched, actualMatched)
 		})
 	}
 }

pkg/vars/vars.go
@@ -24,6 +24,7 @@ const (
 	ApplyAnnotation                                 = Domain + "/last-applied"
 	NodeInterfaceAnnotation                         = Domain + "/configured-interfaces"
 	ManagedByLabel                                  = Domain + "/managed-by"
+	AppliedConfigurationAnnotation                  = Domain + "/applied-configurations"
 	SatelliteNodeLabel                              = Domain + "/linstor-satellite"
 	SatelliteFinalizer                              = Domain + "/satellite-protection"
 	PersistentVolumeWaitForReattachAnnotationPrefix = "wait-for-reattach.evacuation." + Domain

commit b3c5bef91dcf873bee7444e7077a975dca03acff

satellite configuration: reset status every run
Otherwise, the "AppliedTo" list builds up over time with repeated entries of
the same Satellite names.

Signed-off-by: Moritz Wanzenböck <moritz.wanzenboeck@linbit.com>
Fixes: 534ae5b7db5c ("Report additional status for LinstorSatelliteConfiguration")

internal/controller/linstorcluster_controller.go
@@ -225,6 +225,7 @@ func (r *LinstorClusterReconciler) reconcileAppliedResource(ctx context.Context,
 				ObservedGeneration: config.Generation,
 			})
 
+			config.Status.AppliedTo = nil
 			for node, configs := range appliedConfigurations {
 				if slices.Contains(configs, config.Name) {
 					config.Status.AppliedTo = append(config.Status.AppliedTo, node)