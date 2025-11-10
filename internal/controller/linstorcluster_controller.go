piraeusdatastore/piraeus-operator

commit d564cc6deef9807cf571433f04b120509008e740

Report additional status for LinstorSatellite
* Connection/Configuration status
* Deletion Policy
* Pool capacity
* Nr of volumes and snapshots
* Supported layers
* Applied LinstorSatelliteConfigurations

Signed-off-by: Moritz Wanzenböck <moritz.wanzenboeck@linbit.com>

internal/controller/linstorsatellite_controller.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"math"
 	"net"
 	"time"
 
@@ -32,6 +33,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -117,6 +119,7 @@ func (r *LinstorSatelliteReconciler) Reconcile(ctx context.Context, req ctrl.Req
 		}
 	}
 
+	var status piraeusiov1.LinstorSatelliteStatus
 	var deleteErr, stateErr error
 	if lsatellite.GetDeletionTimestamp() != nil {
 		msg, done, err := r.deleteSatellite(ctx, lsatellite, &node, conds)
@@ -133,13 +136,26 @@ func (r *LinstorSatelliteReconciler) Reconcile(ctx context.Context, req ctrl.Req
 			deleteErr = r.Client.Update(ctx, lsatellite)
 		}
 
-		stateErr = r.reconcileLinstorSatelliteState(ctx, lsatellite, &node, conds)
+		stateErr = r.reconcileLinstorSatelliteState(ctx, lsatellite, &node, conds, &status)
 	}
 
 	_, condErr := controllerutil.CreateOrPatch(ctx, r.Client, lsatellite, func() error {
+		status.DeepCopyInto(&lsatellite.Status)
+
 		for _, cond := range conds.ToConditions(lsatellite.Generation) {
 			meta.SetStatusCondition(&lsatellite.Status.Conditions, cond)
 		}
+
+		if status.FreeCapacityBytes != nil && status.TotalCapacityBytes != nil {
+			// Report used/total capacity. Assume "used" is total - free. Always report in GiB.
+			lsatellite.Status.Capacity = fmt.Sprintf("%d/%dGiB",
+				resource.NewQuantity(*status.TotalCapacityBytes-*status.FreeCapacityBytes, resource.BinarySI).ScaledValue(resource.Giga),
+				resource.NewQuantity(*status.TotalCapacityBytes, resource.BinarySI).ScaledValue(resource.Giga),
+			)
+		}
+
+		slices.Sort(lsatellite.Status.StorageProviders)
+		slices.Sort(lsatellite.Status.DeviceLayers)
 
 		return nil
 	})
@@ -316,7 +332,7 @@ func (r *LinstorSatelliteReconciler) kustomizeNodeResources(ctx context.Context,
 	return r.Kustomizer.Kustomize(k)
 }
 
-func (r *LinstorSatelliteReconciler) reconcileLinstorSatelliteState(ctx context.Context, lsatellite *piraeusiov1.LinstorSatellite, node *corev1.Node, conds conditions.Conditions) error {
+func (r *LinstorSatelliteReconciler) reconcileLinstorSatelliteState(ctx context.Context, lsatellite *piraeusiov1.LinstorSatellite, node *corev1.Node, conds conditions.Conditions, status *piraeusiov1.LinstorSatelliteStatus) error {
 	// machine might be nil if
 	// * the MachineClient is nil (integration disabled)
 	// * the cluster is not using ClusterAPI
@@ -438,13 +454,52 @@ func (r *LinstorSatelliteReconciler) reconcileLinstorSatelliteState(ctx context.
 	}
 
 	if lnode.ConnectionStatus == "ONLINE" {
-		conds.AddSuccess(conditions.Available, "satellite online")
+		conds.AddSuccess(conditions.Available, lnode.ConnectionStatus)
 
 		err := r.reconcileStoragePools(ctx, lc, lsatellite, node)
 		if err != nil {
 			conds.AddError(conditions.Configured, err)
 		} else {
 			conds.AddSuccess(conditions.Configured, "Pools configured")
+		}
+
+		// Add additional status information
+		sp, err := lc.Nodes.GetStoragePools(ctx, node.Name)
+		if err == nil {
+			var total, free int64
+			for _, pool := range sp {
+				if pool.TotalCapacity == math.MaxInt64 {
+					// Skip all the "diskless" pools, they always report max capacity
+					continue
+				}
+
+				// LINSTOR reports KiB
+				total += pool.TotalCapacity * 1024
+				free += pool.FreeCapacity * 1024
+			}
+
+			status.TotalCapacityBytes = &total
+			status.FreeCapacityBytes = &free
+		}
+
+		rs, err := lc.Resources.GetResourceView(ctx, &lapi.ListOpts{Node: []string{node.Name}})
+		if err == nil {
+			n := int32(len(rs))
+			status.NumberOfVolumes = &n
+		}
+
+		snaps, err := lc.Resources.GetSnapshotView(ctx, &lapi.ListOpts{Node: []string{node.Name}})
+		if err == nil {
+			n := int32(len(snaps))
+			status.NumberOfSnapshots = &n
+		}
+
+		for _, s := range lnode.StorageProviders {
+			status.StorageProviders = append(status.StorageProviders, string(s))
+		}
+
+		for _, l := range lnode.ResourceLayers {
+			status.DeviceLayers = append(status.DeviceLayers, string(l))
 		}
 
 		if clusterapi.ShouldEvacuateNode(machine) && lsatellite.Spec.DeletionPolicy == piraeusiov1.DeletionPolicyEvacuate {
@@ -464,7 +519,7 @@ func (r *LinstorSatelliteReconciler) reconcileLinstorSatelliteState(ctx context.
 			}
 		}
 	} else {
-		conds.AddError(conditions.Available, fmt.Errorf("satellite not online"))
+		conds.AddError(conditions.Available, fmt.Errorf("%s", lnode.ConnectionStatus))
 	}
 
 	return nil

api/v1/linstorsatellite_types.go
@@ -75,6 +75,34 @@ type LinstorSatelliteStatus struct {
 	// +listType=map
 	// +listMapKey=type
 	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+
+	// The number of volumes on this Satellite.
+	//+kubebuilder:validation:Optional
+	NumberOfVolumes *int32 `json:"numberOfVolumes"`
+
+	// The number of snapshots on this Satellite.
+	//+kubebuilder:validation:Optional
+	NumberOfSnapshots *int32 `json:"numberOfSnapshots"`
+
+	// The number of bytes in total in all storage pools on this Satellite.
+	//+kubebuilder:validation:Optional
+	TotalCapacityBytes *int64 `json:"availableCapacityBytes"`
+
+	// The number of bytes free in all storage pools on this Satellite.
+	//+kubebuilder:validation:Optional
+	FreeCapacityBytes *int64 `json:"freeCapacityBytes"`
+
+	// Capacity mirrors the information from TotalCapacityBytes and FreeCapacityBytes in a human-readable string.
+	//+kubebuilder:validation:Optional
+	Capacity string `json:"capacity"`
+
+	// StorageProviders lists the storage providers (LVM, ZFS, etc...) this Satellite supports.
+	//+kubebuilder:validation:Optional
+	StorageProviders []string `json:"storageProviders,omitempty"`
+
+	// DeviceLayers lists the device layers (LUKS, CACHE, etc...) this Satellite supports.
+	//+kubebuilder:validation:Optional
+	DeviceLayers []string `json:"deviceLayers,omitempty"`
 }
 
 type ClusterReference struct {
@@ -99,6 +127,16 @@ type ClusterReference struct {
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:scope=Cluster
+// +kubebuilder:printcolumn:name="Connected",type=string,JSONPath=`.status.conditions[?(@.type=='Available')].status`,description="If the LINSTOR Satellite is connected"
+// +kubebuilder:printcolumn:name="Configured",type=string,JSONPath=`.status.conditions[?(@.type=='Configured')].status`,description="If the LINSTOR Satellite is fully configured"
+// +kubebuilder:printcolumn:name="Applied Configurations",type=string,JSONPath=`.metadata.annotations.piraeus\.io/applied-configurations`,description="The Satellite Configurations applied to this Satellite",priority=10
+// +kubebuilder:printcolumn:name="Deletion Policy",type=string,JSONPath=`.spec.deletionPolicy`,description="The deletion policy of the Satellite"
+// +kubebuilder:printcolumn:name="Used Capacity",type=string,JSONPath=`.status.capacity`,description="The used capacity on the node"
+// +kubebuilder:printcolumn:name="Volumes",type=integer,JSONPath=`.status.numberOfVolumes`,description="The number of volumes on the node"
+// +kubebuilder:printcolumn:name="Snapshots",type=integer,JSONPath=`.status.numberOfSnapshots`,description="The number of snapshots on the node",priority=10
+// +kubebuilder:printcolumn:name="Storage Providers",type=string,JSONPath=`.status.storageProviders`,description="The storage providers supported by the node",priority=10
+// +kubebuilder:printcolumn:name="Device Layers",type=string,JSONPath=`.status.deviceLayers`,description="The device layers supported by the node",priority=10
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 type LinstorSatellite struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`

api/v1/zz_generated.deepcopy.go
@@ -820,6 +820,36 @@ func (in *LinstorSatelliteStatus) DeepCopyInto(out *LinstorSatelliteStatus) {
 			(*in)[i].DeepCopyInto(&(*out)[i])
 		}
 	}
+	if in.NumberOfVolumes != nil {
+		in, out := &in.NumberOfVolumes, &out.NumberOfVolumes
+		*out = new(int32)
+		**out = **in
+	}
+	if in.NumberOfSnapshots != nil {
+		in, out := &in.NumberOfSnapshots, &out.NumberOfSnapshots
+		*out = new(int32)
+		**out = **in
+	}
+	if in.TotalCapacityBytes != nil {
+		in, out := &in.TotalCapacityBytes, &out.TotalCapacityBytes
+		*out = new(int64)
+		**out = **in
+	}
+	if in.FreeCapacityBytes != nil {
+		in, out := &in.FreeCapacityBytes, &out.FreeCapacityBytes
+		*out = new(int64)
+		**out = **in
+	}
+	if in.StorageProviders != nil {
+		in, out := &in.StorageProviders, &out.StorageProviders
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.DeviceLayers != nil {
+		in, out := &in.DeviceLayers, &out.DeviceLayers
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
 }
 
 // DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new LinstorSatelliteStatus.

charts/piraeus/templates/crds.yaml
@@ -1493,7 +1493,51 @@ spec:
     singular: linstorsatellite
   scope: Cluster
   versions:
-  - name: v1
+  - additionalPrinterColumns:
+    - description: If the LINSTOR Satellite is connected
+      jsonPath: .status.conditions[?(@.type=='Available')].status
+      name: Connected
+      type: string
+    - description: If the LINSTOR Satellite is fully configured
+      jsonPath: .status.conditions[?(@.type=='Configured')].status
+      name: Configured
+      type: string
+    - description: The Satellite Configurations applied to this Satellite
+      jsonPath: .metadata.annotations.piraeus\.io/applied-configurations
+      name: Applied Configurations
+      priority: 10
+      type: string
+    - description: The deletion policy of the Satellite
+      jsonPath: .spec.deletionPolicy
+      name: Deletion Policy
+      type: string
+    - description: The used capacity on the node
+      jsonPath: .status.capacity
+      name: Used Capacity
+      type: string
+    - description: The number of volumes on the node
+      jsonPath: .status.numberOfVolumes
+      name: Volumes
+      type: integer
+    - description: The number of snapshots on the node
+      jsonPath: .status.numberOfSnapshots
+      name: Snapshots
+      priority: 10
+      type: integer
+    - description: The storage providers supported by the node
+      jsonPath: .status.storageProviders
+      name: Storage Providers
+      priority: 10
+      type: string
+    - description: The device layers supported by the node
+      jsonPath: .status.deviceLayers
+      name: Device Layers
+      priority: 10
+      type: string
+    - jsonPath: .metadata.creationTimestamp
+      name: Age
+      type: date
+    name: v1
     schema:
       openAPIV3Schema:
         description: LinstorSatellite is the Schema for the linstorsatellites API
@@ -1938,6 +1982,15 @@ spec:
           status:
             description: LinstorSatelliteStatus defines the observed state of LinstorSatellite
             properties:
+              availableCapacityBytes:
+                description: The number of bytes in total in all storage pools on
+                  this Satellite.
+                format: int64
+                type: integer
+              capacity:
+                description: Capacity mirrors the information from TotalCapacityBytes
+                  and FreeCapacityBytes in a human-readable string.
+                type: string
               conditions:
                 description: Current LINSTOR Satellite state
                 items:
@@ -1998,6 +2051,31 @@ spec:
                 x-kubernetes-list-map-keys:
                 - type
                 x-kubernetes-list-type: map
+              deviceLayers:
+                description: DeviceLayers lists the device layers (LUKS, CACHE, etc...)
+                  this Satellite supports.
+                items:
+                  type: string
+                type: array
+              freeCapacityBytes:
+                description: The number of bytes free in all storage pools on this
+                  Satellite.
+                format: int64
+                type: integer
+              numberOfSnapshots:
+                description: The number of snapshots on this Satellite.
+                format: int32
+                type: integer
+              numberOfVolumes:
+                description: The number of volumes on this Satellite.
+                format: int32
+                type: integer
+              storageProviders:
+                description: StorageProviders lists the storage providers (LVM, ZFS,
+                  etc...) this Satellite supports.
+                items:
+                  type: string
+                type: array
             type: object
         type: object
     served: true

config/crd/bases/piraeus.io_linstorsatellites.yaml
@@ -14,7 +14,51 @@ spec:
     singular: linstorsatellite
   scope: Cluster
   versions:
-  - name: v1
+  - additionalPrinterColumns:
+    - description: If the LINSTOR Satellite is connected
+      jsonPath: .status.conditions[?(@.type=='Available')].status
+      name: Connected
+      type: string
+    - description: If the LINSTOR Satellite is fully configured
+      jsonPath: .status.conditions[?(@.type=='Configured')].status
+      name: Configured
+      type: string
+    - description: The Satellite Configurations applied to this Satellite
+      jsonPath: .metadata.annotations.piraeus\.io/applied-configurations
+      name: Applied Configurations
+      priority: 10
+      type: string
+    - description: The deletion policy of the Satellite
+      jsonPath: .spec.deletionPolicy
+      name: Deletion Policy
+      type: string
+    - description: The used capacity on the node
+      jsonPath: .status.capacity
+      name: Used Capacity
+      type: string
+    - description: The number of volumes on the node
+      jsonPath: .status.numberOfVolumes
+      name: Volumes
+      type: integer
+    - description: The number of snapshots on the node
+      jsonPath: .status.numberOfSnapshots
+      name: Snapshots
+      priority: 10
+      type: integer
+    - description: The storage providers supported by the node
+      jsonPath: .status.storageProviders
+      name: Storage Providers
+      priority: 10
+      type: string
+    - description: The device layers supported by the node
+      jsonPath: .status.deviceLayers
+      name: Device Layers
+      priority: 10
+      type: string
+    - jsonPath: .metadata.creationTimestamp
+      name: Age
+      type: date
+    name: v1
     schema:
       openAPIV3Schema:
         description: LinstorSatellite is the Schema for the linstorsatellites API
@@ -459,6 +503,15 @@ spec:
           status:
             description: LinstorSatelliteStatus defines the observed state of LinstorSatellite
             properties:
+              availableCapacityBytes:
+                description: The number of bytes in total in all storage pools on
+                  this Satellite.
+                format: int64
+                type: integer
+              capacity:
+                description: Capacity mirrors the information from TotalCapacityBytes
+                  and FreeCapacityBytes in a human-readable string.
+                type: string
               conditions:
                 description: Current LINSTOR Satellite state
                 items:
@@ -519,6 +572,31 @@ spec:
                 x-kubernetes-list-map-keys:
                 - type
                 x-kubernetes-list-type: map
+              deviceLayers:
+                description: DeviceLayers lists the device layers (LUKS, CACHE, etc...)
+                  this Satellite supports.
+                items:
+                  type: string
+                type: array
+              freeCapacityBytes:
+                description: The number of bytes free in all storage pools on this
+                  Satellite.
+                format: int64
+                type: integer
+              numberOfSnapshots:
+                description: The number of snapshots on this Satellite.
+                format: int32
+                type: integer
+              numberOfVolumes:
+                description: The number of volumes on this Satellite.
+                format: int32
+                type: integer
+              storageProviders:
+                description: StorageProviders lists the storage providers (LVM, ZFS,
+                  etc...) this Satellite supports.
+                items:
+                  type: string
+                type: array
             type: object
         type: object
     served: true

commit f0a61bbc2cd7c87eaaf4b9584c7a67f89cb354a7

fix excessive reconciles for updated status
When implementing the extended status, we accidentally changed the behaviour
of status conditions: when call DeepCopyInto() on the status object, we reset
the conditions to nil. This caused SetStatusCondition() to set the renew time
of the condition to the current time, causing an update.

As this happened on every reconcile, we effectively created a reconcile loop,
where every reconciliation caused another reconciliation to be queued.

To fix this, copy and update the existing conditions before merging it back
to the status object. This ensures that SetStatusCondition() finds the old
conditions and only updates the conditions on change.

Fixes: d564cc6deef9 ("Report additional status for LinstorSatellite")
Fixes: b9f84ff140d1 ("Report additional status for LinstorCluster")
Signed-off-by: Moritz Wanzenböck <moritz.wanzenboeck@linbit.com>

internal/controller/linstorcluster_controller.go
@@ -142,6 +142,14 @@ func (r *LinstorClusterReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 	status, stateErr := r.reconcileClusterState(ctx, lcluster, conds)
 
 	_, condErr := controllerutil.CreateOrPatch(ctx, r.Client, lcluster, func() error {
+		// Fill in existing conditions so meta.SetStatusCondition does not update the LastTransitionTime if the
+		// condition is already set.
+		status.Conditions = lcluster.Status.Conditions
+
+		for _, cond := range conds.ToConditions(lcluster.Generation) {
+			meta.SetStatusCondition(&status.Conditions, cond)
+		}
+
 		status.DeepCopyInto(&lcluster.Status)
 
 		if status.RunningSatellites != nil && status.ScheduledSatellites != nil {
@@ -154,10 +162,6 @@ func (r *LinstorClusterReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 				resource.NewQuantity(*status.TotalCapacityBytes-*status.FreeCapacityBytes, resource.BinarySI).ScaledValue(resource.Giga),
 				resource.NewQuantity(*status.TotalCapacityBytes, resource.BinarySI).ScaledValue(resource.Giga),
 			)
-		}
-
-		for _, cond := range conds.ToConditions(lcluster.Generation) {
-			meta.SetStatusCondition(&lcluster.Status.Conditions, cond)
 		}
 
 		return nil

internal/controller/linstorsatellite_controller.go
@@ -140,11 +140,15 @@ func (r *LinstorSatelliteReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	}
 
 	_, condErr := controllerutil.CreateOrPatch(ctx, r.Client, lsatellite, func() error {
-		status.DeepCopyInto(&lsatellite.Status)
+		// Fill in existing conditions so meta.SetStatusCondition does not update the LastTransitionTime if the
+		// condition is already set.
+		status.Conditions = lsatellite.Status.Conditions
 
 		for _, cond := range conds.ToConditions(lsatellite.Generation) {
-			meta.SetStatusCondition(&lsatellite.Status.Conditions, cond)
+			meta.SetStatusCondition(&status.Conditions, cond)
 		}
+
+		status.DeepCopyInto(&lsatellite.Status)
 
 		if status.FreeCapacityBytes != nil && status.TotalCapacityBytes != nil {
 			// Report used/total capacity. Assume "used" is total - free. Always report in GiB.